55a148e99f52e4e2172240d937b90d0675a3cc78
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-11-07 12:24:07 +0530

    server js
    server js  error

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import path from 'path'
 import cors from 'cors'
 import recordRoutes from './routes/recordRoutes.js' // Make sure the file extension is .js for ES Modules
 import connectDB from './config/db.js' // Ensure your connectDB file exports the function as default or named export
@@ -8,6 +9,7 @@ dotenv.config()
 const app = express()
 app.use(cors())
 const PORT = process.env.PORT || 3000
+const __dirname = path.resolve()
 
 // Middleware
 app.use(express.json())
@@ -17,6 +19,13 @@ connectDB()
 
 // Routes
 app.use('/api/records', recordRoutes)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
